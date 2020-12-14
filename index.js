1363f472fd756c5ba4c45639762f019777a69464
lnardon <lucaslnardon@gmail.com>
2020-12-14 00:34:02 -0300

    :tada:First commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// IMPORTS
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/GLTFLoader.js";
+
+//SCENE
+const scene = new THREE.Scene();
+
+//RENDERER
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById("canvas"),
+  antialias: true,
+});
+renderer.setClearColor(0x25c8ce);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//CAMERA
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  3000
+);
+
+//LIGHTS
+const light1 = new THREE.AmbientLight(0xffffff, 0.5),
+  light2 = new THREE.PointLight(0xffffff, 1);
+
+scene.add(light1);
+scene.add(light2);
+
+//OBJECT
+const geometry = new THREE.CubeGeometry(100, 100, 100);
+const material = new THREE.MeshLambertMaterial({ color: 0xf3ffe2 });
+const mesh = new THREE.Mesh(geometry, material);
+
+//RENDER LOOP
+requestAnimationFrame(render);
+
+function render() {
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.03;
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+window.addEventListener(
+  "resize",
+  function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  },
+  false
+);
